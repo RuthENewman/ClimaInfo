14ae33b0bfb7cce86d6dbcfd4b68b47b33c3cd79
Ruth Newman <ruthnewman@gmail.com>
2019-05-28 23:52:24 +0100

    switched to celsius

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const request = require('request');
 
-const testURL = "https://api.darksky.net/forecast/bef64b7f9653bd2ecdad26b985f0706b/37.8267,-122.4233";
+const testURL = "https://api.darksky.net/forecast/bef64b7f9653bd2ecdad26b985f0706b/37.8267,-122.4233?units=si";
 
 request({url: testURL, json: true}, (error, response) => {
   console.log('It is currently ' + response.body.currently.temperature
-             + ' degrees. There is a '
+             + ' C. There is a '
              + response.body.currently.precipProbability
              + '% chance of rain.');
 })
