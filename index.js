4d85680fd3df867781a250007a3d0c72f7f6d4a4
Ruth Newman <ruthnewman@gmail.com>
2019-05-28 23:20:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Ready, set, go!')
+
+setTimeout(() => {
+  console.log('Two second timer')
+},2000)
+
+setTimeout(() => {
+  console.log('Zero second timer')
+}, 0)
+
+console.log('Finish line')
