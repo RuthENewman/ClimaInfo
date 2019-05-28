2b1eb8c690fd9407b78b48da9be64d5e9d72c62e
Ruth Newman <ruthnewman@gmail.com>
2019-05-28 23:55:20 +0100

    added daily and currently data

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const request = require('request');
 const testURL = "https://api.darksky.net/forecast/bef64b7f9653bd2ecdad26b985f0706b/37.8267,-122.4233?units=si";
 
 request({url: testURL, json: true}, (error, response) => {
-  console.log('It is currently ' + response.body.currently.temperature
+  const currentlyData = response.body.currently;
+  const dailyData = response.body.daily;
+  console.log('It is currently ' + currentlyData.temperature
              + ' C. There is a '
-             + response.body.currently.precipProbability
+             + currentlyData.precipProbability
              + '% chance of rain.');
 })
