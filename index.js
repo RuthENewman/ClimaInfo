d55ac67d4b7bd48a89527699b699033d7ba203e4
Ruth Newman <ruthnewman@gmail.com>
2019-05-28 23:50:30 +0100

    refactored request

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const request = require('request');
 
 const testURL = "https://api.darksky.net/forecast/bef64b7f9653bd2ecdad26b985f0706b/37.8267,-122.4233";
 
-request({url: testURL}, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.currently);
+request({url: testURL, json: true}, (error, response) => {
+  console.log('It is currently ' + response.body.currently.temperature
+             + ' degrees. There is a '
+             + response.body.currently.precipProbability
+             + '% chance of rain.');
 })
