e4d7209c931e7f2bf767c22fde5c1f37f85368ae
Ruth Newman <ruthnewman@gmail.com>
2019-05-29 14:25:54 +0100

    added request for latitude and longitude

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+// Dark Sky API
+
 const testURL = "https://api.darksky.net/forecast/bef64b7f9653bd2ecdad26b985f0706b/37.8267,-122.4233?units=si";
 
 request({url: testURL, json: true}, (error, response) => {
@@ -10,4 +12,19 @@ request({url: testURL, json: true}, (error, response) => {
              + ' C. There is a '
              + currentlyData.precipProbability
              + '% chance of rain.');
+})
+
+
+// Mapbox geocoding
+
+const mapboxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicnV0aGVuZXdtYW4iLCJhIjoiY2p3OGVvdGw2MmdpYjQzcGx4c3p3bmM1MCJ9.tvwMTbvxPpFxYDABIUiKOw";
+
+const defaultAPIkey = "pk.eyJ1IjoicnV0aGVuZXdtYW4iLCJhIjoiY2p3OGVvdGw2MmdpYjQzcGx4c3p3bmM1MCJ9.tvwMTbvxPpFxYDABIUiKOw"
+
+request({url: mapboxURL, json: true}, (error, response) => {
+  const mapboxData = response.body.features[0];
+  const longitude = mapboxData.center[0];
+  const latitude = mapboxData.center[1];
+  console.log(longitude)
+  console.log(latitude)
 })
